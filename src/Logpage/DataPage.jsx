d7062a91ed56f40e943b6f940b64ddd87afb3c03
Prathiv raj <prathivraj.dev01@gmail.com>
2026-02-13 10:08:08 +0530

    Add files via upload

---
+++ src/Logpage/DataPage.jsx
@@ -0,0 +1,15 @@
+function DataPage(){
+    return(
+        <div>
+            <div>
+                <table>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Password</th>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    );
+}
